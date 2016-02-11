742969459be63da07715834067a720abd139e916
naverstay <3a4f044785G->
2016-02-11 08:50:11 +0200

    notify animation

--- www/js/edit_patient_script.js
+++ www/js/edit_patient_script.js
@@ -43,22 +43,50 @@ $(function ($) {
         html: "<div>" +
         " <div class='clearfix'> <p>" +
         " <span class='title' data-notify-html='title'/>" +
-        " <a href='#' class='attach_rm_cancel gl_link white_link'><span>отменить</span></a>" +  
+        " <a href='#' class='attach_rm_cancel gl_link white_link'><span>отменить</span></a>" +
         " </p></div>" +
         " </div>"
     });
 
     $('.rmAttachBtn').on('click', function () {
 
-        $.notify({
-            title: 'Фотография rentgen-10-12-2015.jpg удалена, '
-        }, {
-            style: 'attach',
-            autoHide: false,
-            clickToHide: false,
-            className: 'removed',
-            autoHideDelay: 5000
-        });
+        var firedEl = $(this).closest('.attach_item');
+
+        firedEl.addClass('removed');
+
+        setTimeout(function () {
+
+            firedEl.hide();
+
+            $.notify({
+                title: 'Фотография rentgen-10-12-2015.jpg удалена, '
+            }, {
+                style: 'attach',
+                autoHide: true,
+                clickToHide: false,
+                arrowShow: false,
+                className: 'removed',
+                showAnimation: 'slideDown',
+                // show animation duration
+                showDuration: 400,
+                // hide animation
+                hideAnimation: 'slideUp',
+                // hide animation duration
+                hideDuration: 200,
+                autoHideDelay: 5000,
+                destroyDelay: 500,
+                onShow: function (e) {
+                    setTimeout(function () {
+                        e.wrapper.addClass('opened');
+                    }, 3);
+                },
+                beforeHide: function (e) {
+                    e.wrapper.addClass('closed');
+                }
+            });
+
+        }, 800);
+
 
         return false;
     });
